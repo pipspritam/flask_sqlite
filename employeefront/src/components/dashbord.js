7342105b146b82186b5bbaf6ea140e9a1f100486
Pritam Mondal <pritammondalpips@gmail.com>
2024-02-15 23:13:28 +0530

    final almost

--- employeefront/src/components/dashbord.js
+++ employeefront/src/components/dashbord.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Col, Row, Card,  CardBody, CardText, CardTitle} from 'reactstrap';
+import { Col, Row, Card, CardBody, CardText, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
-import AddchartIcon from '@mui/icons-material/Addchart';
 import VerticalSplitOutlinedIcon from '@mui/icons-material/VerticalSplitOutlined';
 import QueryStatsOutlinedIcon from '@mui/icons-material/QueryStatsOutlined';
 import './dashboardCard.css';
@@ -10,202 +8,118 @@ import { Avatar } from '@mui/material';
 import DonutSmallIcon from '@mui/icons-material/DonutSmall';
 import ReportProblemIcon from '@mui/icons-material/ReportProblem';
 import IosShareIcon from '@mui/icons-material/IosShare';
+import SpeedIcon from '@mui/icons-material/Speed';
+import StorageIcon from '@mui/icons-material/Storage';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import HubIcon from '@mui/icons-material/Hub';
+import SettingsInputCompositeIcon from '@mui/icons-material/SettingsInputComposite';
 
 
 const firstQs = [
     {
         name: 'Capacity Metrics',
-        src: <DonutSmallIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Reports' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
+        src: <DonutSmallIcon style={{ fontSize: '80px' }} />,
+        details: 'Capacity Metrics shows the available and used capacity of the storage system.',
         link: '/'
     },
     {
         name: 'Performance Metrics',
-        src: <AddchartIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Charts' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: ''
+        src: <SpeedIcon style={{ fontSize: '80px' }} />,
+        details: 'Performance Metrics shows the performance data of the storage system.',
+        link: '/'
     },
     {
         name: 'Alerts',
-        src: <ReportProblemIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Others' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: ''
+        src: <ReportProblemIcon style={{ fontSize: '80px' }} />,
+        details: 'Alerts display notifications about system errors or warnings.',
+        link: '/'
     },
     {
         name: 'CIFS/Windows shares',
-        src: <IosShareIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Reports' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
+        src: <IosShareIcon style={{ fontSize: '80px' }} />,
+        details: 'CIFS/Windows shares provide access to files on the network.',
+        link: '/'
+    }, {
+        name: 'SAN Storage',
+        src: <StorageIcon style={{ fontSize: '80px' }} />,
+        details: 'SAN Storage provides high-speed data storage.',
+        link: '/'
+    },
+    {
+        name: 'NFS/Linux Shares',
+        src: <VerticalSplitOutlinedIcon style={{ fontSize: '80px' }} />,
+        details: 'NFS/Linux Shares provide access to files for Linux systems.',
+        link: '/'
+    },
+    {
+        name: 'Object Storage(S3)',
+        src: <Inventory2Icon style={{ fontSize: '80px' }} />,
+        details: 'Object Storage(S3) offers scalable storage for large amounts of data.',
+        link: '/'
+    },
+    {
+        name: 'Storage Archival',
+        src: <ArchiveIcon style={{ fontSize: '80px' }} />,
+        details: 'Storage Archival is used for long-term data retention.',
+        link: '/'
+    },
+    {
+        name: 'SAN - Switches / Zoning',
+        src: <SettingsInputCompositeIcon style={{ fontSize: '80px' }} />,
+        details: 'SAN - Switches / Zoning manage connections in a storage area network.',
+        link: '/'
+    }, {
+        name: 'SLI',
+        src: <QueryStatsOutlinedIcon style={{ fontSize: '80px' }} />,
+        details: 'SLI (Service Level Indicator) monitors the performance of services.',
+        link: '/'
+    },
+    {
+        name: 'Topology',
+        src: <HubIcon style={{ fontSize: '80px' }} />,
+        details: 'Topology shows the arrangement of network devices.',
         link: '/'
     }
 ];
 
-const quickLinks = [
-    {
-        name: 'SAN Storage',
-        src: <VerticalSplitOutlinedIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Reports' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: '/reports'
-    },
-    {
-        name: 'NFS/Linux Shares',
-        src: <VerticalSplitOutlinedIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Reports' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: '/reports'
-    },
-    {
-        name: 'Object Storage(S3)',
-        src: <VerticalSplitOutlinedIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Reports' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: '/reports'
-    },
-    {
-        name: 'Storage Archival',
-        src: <VerticalSplitOutlinedIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Reports' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: '/reports'
-    },
-    {
-        name: 'SAN - Switches / Zoning',
-        src: <VerticalSplitOutlinedIcon style={{ fontSize: '100px' }} />,
-        text: [{ name: 'Reports' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: '/reports'
-    }
-];
-
-const serviceCatalogs = [
-    {
-        name: 'SLI',
-        src: <QueryStatsOutlinedIcon style={{ fontSize: '200px' }} />,
-        text: [{ name: 'Others' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: '/approval'
-    },
-    {
-        name: 'Topology',
-        src: <QueryStatsOutlinedIcon style={{ fontSize: '200px' }} />,
-        text: [{ name: 'Others' }],
-        details: 'Capacity Metrics shows the capacity of the storage system and the capacity of the storage system that is used. ',
-        link: '/approval'
-    }
-];
 
 function Dashboard() {
     return (
-        <div>
-            <div className="firstline" style={{ marginTop: '3em' }}>
-                <Row className="row">
-                    {firstQs.map((firstQ) => (
-                        <Col xs="12" sm="6" lg="4" key={firstQ.name}>
-                            <Link to={firstQ.link} style={{ textDecoration: 'none', color: 'inherit', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <Card className="card">
-                                    <CardBody className="body">
-                                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', justifyContent: 'center' }}>
-                                            <div style={{ height: '30%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                                <Avatar
-                                                    sx={{
-                                                        width: '70px',
-                                                        height: '70px',
-                                                        borderRadius: '0',
-                                                        bgcolor: 'transparent',
-                                                        color: '#048888d3'
-                                                    }}
-                                                >
-                                                    {firstQ.src}
-                                                </Avatar>
-                                            </div>
-                                            <div style={{ textAlign: 'center', marginTop: '1em' }}>
-                                                <CardTitle tag="h4">{firstQ.name}</CardTitle>
-                                                <CardText style={{ textAlign: 'left' }}>{firstQ.details}</CardText> {/* Updated alignment */}
-                                            </div>
+        <div className="firstline" style={{ marginTop: '3em' }}>
+            <Row className="row">
+                {firstQs.map((firstQ, index) => (
+                    <Col key={index} xs="12" sm="6" lg="4">
+                        <Link to={firstQ.link} style={{ textDecoration: 'none', color: 'inherit', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <Card className="card">
+                                <CardBody className="body">
+                                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', justifyContent: 'center' }}>
+                                        <div style={{ height: '30%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '10px' }}>
+                                            <Avatar
+                                                sx={{
+                                                    width: '70px',
+                                                    height: '70px',
+                                                    bgcolor: 'transparent',
+                                                    color: '#048888d3',
+                                                    padding: '10px'
+                                                }}
+                                            >
+                                                {firstQ.src}
+                                            </Avatar>
                                         </div>
-                                    </CardBody>
-                                </Card>
-                            </Link>
-                        </Col>
-                    ))}
-                </Row>
-            </div>
-            <div className="secondLine" style={{ marginTop: '3em' }}>
-                <Row className="row">
-                    {quickLinks.map((quicklink) => (
-                        <Col xs="12" sm="6" lg="4" key={quicklink.name}>
-                            <Link to={quicklink.link} style={{ textDecoration: 'none', color: 'inherit', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <Card className="card secondCard">
-                                    <CardBody className="body">
-                                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', justifyContent: 'center' }}>
-                                            <div style={{ height: '30%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                                <Avatar
-                                                    sx={{
-                                                        width: '70px',
-                                                        height: '70px',
-                                                        borderRadius: '0',
-                                                        bgcolor: 'transparent',
-                                                        color: '#048888d3'
-                                                    }}
-                                                >
-                                                    {quicklink.src}
-                                                </Avatar>
-                                            </div>
-                                            <div style={{ textAlign: 'center', marginTop: '1em' }}>
-                                                <CardTitle tag="h4">{quicklink.name}</CardTitle>
-                                                <CardText style={{ textAlign: 'left' }}>{quicklink.details}</CardText> {/* Updated alignment */}
-                                            </div>
+                                        <div style={{ textAlign: 'center', marginTop: '1em' }}>
+                                            <CardTitle tag="h4" style={{ fontSize: '1.2rem' }}>{firstQ.name}</CardTitle>
+                                            <CardText>{firstQ.details}</CardText>
                                         </div>
-                                    </CardBody>
-                                </Card>
-                            </Link>
-                        </Col>
-                    ))}
-                </Row>
-            </div>
-            <div className="newTable" style={{ marginTop: '3em' }}>
-                <Row className="row">
-                    {serviceCatalogs.map((servicecatalog) => (
-                        <Col xs="12" sm="6" lg="4" key={servicecatalog.name}>
-                            <Link to={servicecatalog.link} style={{ textDecoration: 'none', color: 'inherit', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <Card className="card thirdCard">
-                                    <CardBody className="body">
-                                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', justifyContent: 'center' }}>
-                                            <div style={{ height: '30%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                                <Avatar
-                                                    sx={{
-                                                        width: '70px',
-                                                        height: '70px',
-                                                        borderRadius: '0',
-                                                        bgcolor: 'transparent',
-                                                        color: '#048888d3'
-                                                    }}
-                                                >
-                                                    {servicecatalog.src}
-                                                </Avatar>
-                                            </div>
-                                            <div style={{ textAlign: 'center', marginTop: '1em' }}>
-                                                <CardTitle tag="h4">{servicecatalog.name}</CardTitle>
-                                                <CardText style={{ textAlign: 'left' }}>{servicecatalog.details}</CardText> {/* Updated alignment */}
-                                            </div>
-                                        </div>
-                                    </CardBody>
-                                </Card>
-                            </Link>
-                        </Col>
-                    ))}
-                </Row>
-            </div>
+                                    </div>
+                                </CardBody>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
 }
-
-
-
-
 
 export default Dashboard;
