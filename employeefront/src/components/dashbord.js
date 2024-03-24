605cc8be8dc44ed7b877b428d7d2fa947a6fff9b
Pritam Mondal <pritammondalpips@gmail.com>
2024-03-24 23:46:40 +0530

    card

--- employeefront/src/components/dashbord.js
+++ employeefront/src/components/dashbord.js
@@ -1,206 +1,79 @@
 import React from "react";
-import { Col, Row, Card, CardBody, CardText, CardTitle } from "reactstrap";
-import { Link } from "react-router-dom";
-import VerticalSplitOutlinedIcon from "@mui/icons-material/VerticalSplitOutlined";
-import QueryStatsOutlinedIcon from "@mui/icons-material/QueryStatsOutlined";
-import "./dashboardCard.css";
-import { Avatar } from "@mui/material";
-import DonutSmallIcon from "@mui/icons-material/DonutSmall";
-import ReportProblemIcon from "@mui/icons-material/ReportProblem";
-import IosShareIcon from "@mui/icons-material/IosShare";
-import SpeedIcon from "@mui/icons-material/Speed";
-import StorageIcon from "@mui/icons-material/Storage";
-import Inventory2Icon from "@mui/icons-material/Inventory2";
-import ArchiveIcon from "@mui/icons-material/Archive";
-import HubIcon from "@mui/icons-material/Hub";
-import SettingsInputCompositeIcon from "@mui/icons-material/SettingsInputComposite";
+import { Grid, Typography, Link, Paper } from "@mui/material";
+import serverSVG from "./server.svg";
+
 
 const firstQs = [
-  {
-    name: "Capacity Metrics",
-    src: <DonutSmallIcon style={{ fontSize: "80px" }} />,
-    details:
-      "Capacity Metrics shows the available and used capacity of the storage system.",
-    link: "/",
-  },
-  {
-    name: "Performance Metrics",
-    src: <SpeedIcon style={{ fontSize: "80px" }} />,
-    details:
-      "Performance Metrics shows the performance data of the storage system.",
-    link: "/",
-  },
-  {
-    name: "Alerts",
-    src: <ReportProblemIcon style={{ fontSize: "80px" }} />,
-    details: "Alerts display notifications about system errors or warnings.",
-    link: "/",
-  },
-  {
-    name: "CIFS/Windows shares",
-    src: <IosShareIcon style={{ fontSize: "80px" }} />,
-    details: "CIFS/Windows shares provide access to files on the network.",
-    link: "/",
-  },
-  {
-    name: "SAN Storage",
-    src: <StorageIcon style={{ fontSize: "80px" }} />,
-    details: "SAN Storage provides high-speed data storage.",
-    link: "/",
-  },
-  {
-    name: "NFS/Linux Shares",
-    src: <VerticalSplitOutlinedIcon style={{ fontSize: "80px" }} />,
-    details: "NFS/Linux Shares provide access to files for Linux systems.",
-    link: "/",
-  },
-  {
-    name: "Object Storage(S3)",
-    src: <Inventory2Icon style={{ fontSize: "80px" }} />,
-    details:
-      "Object Storage(S3) offers scalable storage for large amounts of data.",
-    link: "/",
-  },
-  {
-    name: "Storage Archival",
-    src: <ArchiveIcon style={{ fontSize: "80px" }} />,
-    details: "Storage Archival is used for long-term data retention.",
-    link: "/",
-  },
-  {
-    name: "SAN - Switches / Zoning",
-    src: <SettingsInputCompositeIcon style={{ fontSize: "80px" }} />,
-    details:
-      "SAN - Switches / Zoning manage connections in a storage area network.",
-    link: "/",
-  },
-  {
-    name: "SLI",
-    src: <QueryStatsOutlinedIcon style={{ fontSize: "80px" }} />,
-    details:
-      "SLI (Service Level Indicator) monitors the performance of services.",
-    link: "/",
-  },
-  {
-    name: "Topology",
-    src: <HubIcon style={{ fontSize: "80px" }} />,
-    details: "Topology shows the arrangement of network devices.",
-    link: "/",
-  },
+  "Capacity Metrics",
+  "Performance Metrics",
+  "Alerts",
+  "CIFS/Windows shares",
+  "SAN Storage",
+  "NFS/Linux Shares",
+  "Object Storage(S3)",
+  "Storage Archival",
+  "SAN - Switches / Zoning",
+  "SLI",
+  "Topology",
 ];
 
 function Dashboard() {
   return (
-    <>
-      {/* <div className="firstline" style={{ marginTop: "3em" }}>
-        <Row className="row">
-          {firstQs.map((firstQ, index) => (
-            <Col key={index} xs="12" sm="6" lg="4">
-              <Link
-                to={firstQ.link}
-                style={{
-                  textDecoration: "none",
-                  color: "inherit",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Card className="card">
-                  <CardBody className="body">
-                    <div
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        height: "100%",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <div
-                        style={{
-                          height: "30%",
-                          width: "100%",
-                          display: "flex",
-                          justifyContent: "center",
-                          alignItems: "center",
-                          paddingTop: "10px",
-                        }}
-                      >
-                        <Avatar
-                          sx={{
-                            width: "70px",
-                            height: "70px",
-                            bgcolor: "transparent",
-                            color: "#048888d3",
-                            padding: "10px",
-                          }}
-                        >
-                          {firstQ.src}
-                        </Avatar>
-                      </div>
-                      <div style={{ textAlign: "center", marginTop: "1em" }}>
-                        <CardTitle tag="h4" style={{ fontSize: "1.2rem" }}>
-                          {firstQ.name}
-                        </CardTitle>
-                        <CardText>{firstQ.details}</CardText>
-                      </div>
-                    </div>
-                  </CardBody>
-                </Card>
-              </Link>
-            </Col>
-          ))}
-        </Row>
-      </div> */}
-
-      <div className=" grid grid-cols-1 md:grid-cols-3  gap-4 p-8">
-        {firstQs.map((firstQ, index) => {
-          return (
-            <div className=" bg-white transition-all duration-200 shadow-sm hover:shadow-xl hover:shadow-[#04888833] p-8 border border-[#048888d3] rounded-lg flex items-center justify-between">
-              <div className="max-w-96">
-                <h1 className=" text-[#048888d3] text-2xl font-semibold mb-2">
-                  Client Access Management
-                </h1>
-                <p className=" mb-4 font-medium text-gray-600">
-                  You have done 72% more sales today. Check your new badge in
-                  your profile.
-                </p>
-                <a
-                  href={"/"}
-                  className="group relative inline-flex items-center overflow-hidden rounded border border-current px-8 py-3 text-[#048888d3] focus:outline-none focus:ring active:text-[#048888d3] hover:bg-[#048888d3] hover:text-white"
-                >
-                  <span className="absolute -start-full transition-all group-hover:start-4">
-                    <svg
-                      class="size-5 rtl:rotate-180"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M17 8l4 4m0 0l-4 4m4-4H3"
-                      />
-                    </svg>
-                  </span>
-
-                  <span className="text-sm font-medium transition-all group-hover:ms-4">
-                    {" "}
-                    Manage Access{" "}
-                  </span>
-                </a>
+    <Grid container spacing={4} sx={{ padding: 8 }}>
+      {firstQs.map((firstQ, index) => {
+        return (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Paper
+              elevation={3}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                height: "100%",
+                width: "100%",
+                transition: "all 0.2s",
+                "&:hover": {
+                  boxShadow: "0px 0px 8px rgba(4, 136, 136, 0.2)",
+                },
+              }}
+            >
+              <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", width: "50%", padding: "16px" }}>
+                <div>
+                  <Typography variant="h5" sx={{ color: "#048888d3", mb: 2 }}>
+                    {firstQ}
+                  </Typography>
+                  <Typography variant="body1" sx={{ mb: 2, color: "gray" }}>
+                    {`Details for ${firstQ}`}
+                  </Typography>
+                </div>
+                <div>
+                  <Link
+                    href="/"
+                    underline="none"
+                    sx={{
+                      display: "inline-flex",
+                      alignItems: "center",
+                      border: "1px solid #048888d3",
+                      borderRadius: 4,
+                      p: "8px 16px",
+                      color: "#048888d3",
+                      "&:hover": {
+                        backgroundColor: "#048888d3",
+                        color: "white",
+                      },
+                    }}
+                  >
+                    Manage Access
+                  </Link>
+                </div>
               </div>
-              <div className=" w-40 ">
-                <img src="/server.svg" alt="" className=" w-full h-auto" />
+              <div style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "50%" }}>
+                <img src={serverSVG} alt="Server" style={{ maxWidth: "100%", maxHeight: "100%" }} />
               </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
+            </Paper>
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 }
 
